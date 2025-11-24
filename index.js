736de3eb7addcdd2caae74cd6a0d7ef4c321ddcf
hisoka <hisoka>
2025-11-24 16:41:27 +0100

    init

---
+++ index.js
@@ -0,0 +1,32 @@
+import express from "express";
+import morgan from "morgan";
+
+const app = express();
+
+app.use(morgan('dev'))
+
+app.set("views", './views');
+app.set("view engine", 'ejs');
+
+app.get("/", (req, res) => {
+  res.send("Hello From Node.js!");
+});
+
+app.get("/contact", (req, res) => {
+  res.send("The Contact Page");
+});
+
+app.get("/about", (req, res) => {
+  res.send("The About Page");
+});
+
+app.get('/*splat', (req, res) => {
+  res.status(404).send('Not Found');
+});
+
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
